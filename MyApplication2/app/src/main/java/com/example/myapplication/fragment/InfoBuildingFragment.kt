bfc49954a59e7d1aaf163152d47243cd84896b57
AmandaFabriano <amanda.fabriano@flexbuilding.tech>
2022-10-18 17:43:35 +0200

    first commit

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/fragment/InfoBuildingFragment.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.fragment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class InfoBuildingFragment : Fragment(){
+    override fun onCreateView(inflater:LayoutInflater,
+                              container:ViewGroup?):
+      }
+
+
