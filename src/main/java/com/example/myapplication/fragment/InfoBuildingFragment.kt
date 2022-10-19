285faa99b0e2e576523d21a8e460ed8b6c044e48
AmandaFabriano <115981476+AmandaFabriano@users.noreply.github.com>
2022-10-19 12:39:19 +0200

    Add files via upload

---
+++ src/main/java/com/example/myapplication/fragment/InfoBuildingFragment.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class InfoBuildingFragment: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.fragment_info_building)
+}
+}
