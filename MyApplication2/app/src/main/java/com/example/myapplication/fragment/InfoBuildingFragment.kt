56f3f0e91b25376fce9d02549f2694b412e27742
ipcrea <hermann@ipstand.fr>
2022-10-19 10:29:39 +0200

    correction rman

--- MyApplication2/app/src/main/java/com/example/myapplication/fragment/InfoBuildingFragment.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/fragment/InfoBuildingFragment.kt
@@ -1,12 +1,48 @@
 package com.example.myapplication.fragment
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.R
 
 class InfoBuildingFragment : Fragment(){
-    override fun onCreateView(inflater:LayoutInflater,
-                              container:ViewGroup?):
-      }
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
 
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_toto_blank, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment TotoBlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            InfoBuildingFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
